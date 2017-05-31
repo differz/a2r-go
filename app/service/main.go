f65c095d50ebbf4ecedb9a1b1254caf628f24503
differz <differ@xlan.net.ua>
2017-05-31 11:57:45 +0300

    Add simple service on 8080

--- app/service/main.go
+++ app/service/main.go
@@ -1,10 +1,41 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"../../config"
+	"../../data/phonebook"
+	"../../manager"
+
 	_ "github.com/Go-SQL-Driver/MySQL"
 	_ "github.com/lib/pq"
 )
 
-func main() {
+func handler(w http.ResponseWriter, r *http.Request) {
 
+	fmt.Fprintf(w, "Server request, URL %s", r.URL.Path[1:])
+
+	cfg := config.New()
+	err := cfg.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	phonebook.Get()
+
+	manager := manager.New()
+
+	store, err := manager.Create()
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return
+	}
+
+	_ = store
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
